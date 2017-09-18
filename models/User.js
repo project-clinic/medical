79d808fed0c7c58630facf9122ddf06690199465
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-09-18 16:01:38 +0200

    User model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema   = mongoose.Schema
+
+const userSchema = new Schema ({
+  name: String,
+  surname: String,
+  password: String,
+  email: String,
+  role: {
+    type: String,
+    enum: ['Admin', 'Doctor', 'Patient'],
+    default: 'Patient'
+  },
+  contact: {
+    address: String,
+    phone: Number,
+  },
+  personaldata: {
+    birthday: Date,
+    gender: {
+      type: String,
+      enum: ['Female', 'Male'],
+      default: 'Female'
+    },
+    height: Number,
+    weight: Number
+  },
+  background: String
+})
+
+userSchema.set('timestamps', true)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
