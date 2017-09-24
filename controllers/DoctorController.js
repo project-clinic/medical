0ae340be3e62e99e81d0602902ef221fbe42c39e
Mercedes Egido <meegido@gmail.com>
2017-09-24 10:20:36 +0200

    fixed new doctor with collegiate number

--- controllers/DoctorController.js
+++ controllers/DoctorController.js
@@ -34,9 +34,10 @@ module.exports = {
 
         const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
         const newUser = new User({
-          name, surname, idCard, collegiate,
+          name, surname, idCard, 
           professional: {
-            speciality
+            speciality,
+            collegiate
           },
           password: hashPass,
           role: 'Doctor'
